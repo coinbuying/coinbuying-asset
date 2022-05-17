1a4e2ed6263f427c8ec5f533a1c8d70c61da906a
huisoo <huiwater@gmail.com>
2022-05-17 20:01:24 +0900

    유레카 클라이언트 등록

--- src/main/java/coinbuying/coinbuyingasset/CoinbuyingAssetApplication.java
+++ src/main/java/coinbuying/coinbuyingasset/CoinbuyingAssetApplication.java
@@ -2,7 +2,9 @@ package coinbuying.coinbuyingasset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CoinbuyingAssetApplication {
 
